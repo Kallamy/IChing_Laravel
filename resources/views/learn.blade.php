3d5a344ab697aa85d3995be3f4f46659512620a3
Yuri Mallak <yurimallak@gmail.com>
2024-01-12 18:43:30 +0200

    creating learn view

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -1,5 +1,20 @@
 <x-layout page="learn">
     <section id="learn">
-        <h2>Informações sobre o I Ching</h2>
+        <nav class="learnMenu">
+            <ul>
+                <a href={{route("learn.history")}}><li class={{ request()->routeIs('learn') || request()->routeIs('learn.history')  ? 'active' : ''}}>História do I Ching</li></a></a>
+                <a href={{route("learn.about")}}><li class={{ request()->routeIs('learn.about') ? 'active' : ''}}>Sobre o I Ching</li></a></a>
+                <a href={{route("learn.trigrams")}}><li class={{ request()->routeIs('learn.trigrams') ? 'active' : ''}}>Trigramas</li></a></a>
+                <a href={{route("learn.hexagrams")}}><li class={{ request()->routeIs('learn.hexagrams') ? 'active' : ''}}>Hexagramas</li></a></a>
+            </ul>
+
+        </nav>
+        <div class="learnContainer">
+            {!!$content!!}
+        </div>
     </section>
 </x-layout>
+<script>
+
+
+</script>
--- resources/views/learn/hexagrams.blade.php
+++ resources/views/learn/hexagrams.blade.php
@@ -10,325 +10,382 @@
         <div>O Receptivo</div>
     </div>
 @endfor --}}
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷀</div>
-        <div>O Receptivo</div>
+    <div class="hexagramIndex">
+
+    </div>
+    <div class="hexagramIndex">
+        ☰
+    </div>
+    <div class="hexagramIndex">
+        ☷
+    </div>
+    <div class="hexagramIndex">
+        ☳
+    </div>
+    <div class="hexagramIndex">
+        ☵
+    </div>
+    <div class="hexagramIndex">
+        ☶
+    </div>
+    <div class="hexagramIndex">
+        ☴
+    </div>
+    <div class="hexagramIndex">
+        ☲
+    </div>
+    <div class="hexagramIndex">
+        ☱
+    </div>
+    <div class="hexagramIndex">
+        ☰
     </div>
     <div class="hexagramItem">
         <div>Ch'ien</div>
+        <div class="hexagramSymbol">䷀</div>
+        <div>O Criativo</div>
+    </div>
+    <div class="hexagramItem">
+        <div>T'ai</div>
+        <div class="hexagramSymbol">䷊</div>
+        <div>Paz</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ta Chuang</div>
+        <div class="hexagramSymbol">䷡</div>
+        <div>O Poder do Grande</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Hsu</div>
+        <div class="hexagramSymbol">䷄</div>
+        <div>A Espera</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ta Ch'u</div>
+        <div class="hexagramSymbol">䷙</div>
+        <div>O Poder de Domar do Grande</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Hsiao Ch'u</div>
+        <div class="hexagramSymbol">䷈</div>
+        <div>O Poder de Domar do Pequeno</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ta Yu</div>
+        <div class="hexagramSymbol">䷍</div>
+        <div>Grandes Posses</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Kuai</div>
+        <div class="hexagramSymbol">䷪</div>
+        <div>Irromper</div>
+    </div>
+    <div class="hexagramIndex">
+        ☷
+    </div>
+    <div class="hexagramItem">
+        <div>Pi</div>
+        <div class="hexagramSymbol">䷋</div>
+        <div>Estagnação</div>
+    </div>
+    <div class="hexagramItem">
+        <div>K'un</div>
         <div class="hexagramSymbol">䷁</div>
         <div>O Receptivo</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷂</div>
-        <div>O Receptivo</div>
+        <div>Yu</div>
+        <div class="hexagramSymbol">䷏</div>
+        <div>Entusiasmo</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷃</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷄</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷅</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷆</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
+        <div>Pi</div>
         <div class="hexagramSymbol">䷇</div>
-        <div>O Receptivo</div>
+        <div>Manter-se Unido</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷈</div>
-        <div>O Receptivo</div>
+        <div>Po</div>
+        <div class="hexagramSymbol">䷖</div>
+        <div>Desintegração</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷉</div>
-        <div>O Receptivo</div>
+        <div>Kuan</div>
+        <div class="hexagramSymbol">䷓</div>
+        <div>Contemplação</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷊</div>
-        <div>O Receptivo</div>
+        <div>Chin</div>
+        <div class="hexagramSymbol">䷢</div>
+        <div>Progresso</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷋</div>
-        <div>O Receptivo</div>
+        <div>Ts'ui</div>
+        <div class="hexagramSymbol">䷬</div>
+        <div>Reunião</div>
+    </div>
+    <div class="hexagramIndex">
+        ☳
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷌</div>
-        <div>O Receptivo</div>
+        <div>Wu Wang</div>
+        <div class="hexagramSymbol">䷘</div>
+        <div>Inocência</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷍</div>
-        <div>O Receptivo</div>
+        <div>Fu</div>
+        <div class="hexagramSymbol">䷗</div>
+        <div>Retorno</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chên</div>
+        <div class="hexagramSymbol">䷲</div>
+        <div>O Incitar</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chun</div>
+        <div class="hexagramSymbol">䷂</div>
+        <div>Dificulade Inicial</div>
+    </div>
+    <div class="hexagramItem">
+        <div>I</div>
+        <div class="hexagramSymbol">䷚</div>
+        <div>As Bordas da Boca</div>
+    </div>
+    <div class="hexagramItem">
+        <div>I</div>
+        <div class="hexagramSymbol">䷩</div>
+        <div>Aumento</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Shih Ho</div>
+        <div class="hexagramSymbol">䷔</div>
+        <div>Morder</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Sui</div>
+        <div class="hexagramSymbol">䷐</div>
+        <div>Seguir</div>
+    </div>
+    <div class="hexagramIndex">
+        ☵
+    </div>
+    <div class="hexagramItem">
+        <div>Sung</div>
+        <div class="hexagramSymbol">䷅</div>
+        <div>Conflito</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Shih</div>
+        <div class="hexagramSymbol">䷆</div>
+        <div>O Exército</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Hsieh</div>
+        <div class="hexagramSymbol">䷧</div>
+        <div>Liberação</div>
+    </div>
+    <div class="hexagramItem">
+        <div>K'an</div>
+        <div class="hexagramSymbol">䷜</div>
+        <div>O Abismal</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Meng</div>
+        <div class="hexagramSymbol">䷃</div>
+        <div>Insensatez Juvenil</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Huan</div>
+        <div class="hexagramSymbol">䷺</div>
+        <div>Dispersão</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Wei Chi</div>
+        <div class="hexagramSymbol">䷿</div>
+        <div>Antes da Conclusão</div>
+    </div>
+    <div class="hexagramItem">
+        <div>K'un</div>
+        <div class="hexagramSymbol">䷮</div>
+        <div>Opressão</div>
+    </div>
+    <div class="hexagramIndex">
+        ☶
+    </div>
+    <div class="hexagramItem">
+        <div>Tun</div>
+        <div class="hexagramSymbol">䷠</div>
+        <div>A Retirada</div>
     </div>
     <div class="hexagramItem">
         <div>Ch'ien</div>
         <div class="hexagramSymbol">䷎</div>
-        <div>O Receptivo</div>
+        <div>Modéstia</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷏</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷐</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷑</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷒</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷓</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷔</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷕</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷖</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷗</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷘</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷙</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷚</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷛</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷜</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷝</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷞</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷟</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷠</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷡</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷢</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷣</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷤</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷥</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷦</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷧</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷨</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷩</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷪</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷫</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷬</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷭</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷮</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷯</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷰</div>
-        <div>Trabalho sobre o que se deteriorou</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷱</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷲</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷳</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷴</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷵</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷶</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷷</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷸</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷹</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷺</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷻</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷼</div>
-        <div>O Receptivo</div>
-    </div>
-    <div class="hexagramItem">
-        <div>Ch'ien</div>
+        <div>Hsiao Kuo</div>
         <div class="hexagramSymbol">䷽</div>
-        <div>O Receptivo</div>
+        <div>A Preponderância do Pequeno</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
+        <div>Chien</div>
+        <div class="hexagramSymbol">䷦</div>
+        <div>Obstrução</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Kên</div>
+        <div class="hexagramSymbol">䷳</div>
+        <div>A Quietude</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chien</div>
+        <div class="hexagramSymbol">䷴</div>
+        <div>Desenvolvimento</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Lü</div>
+        <div class="hexagramSymbol">䷷</div>
+        <div>O Viajante</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Hsien</div>
+        <div class="hexagramSymbol">䷞</div>
+        <div>A Influência</div>
+    </div>
+    <div class="hexagramIndex">
+        ☴
+    </div>
+    <div class="hexagramItem">
+        <div>Kou</div>
+        <div class="hexagramSymbol">䷫</div>
+        <div>Vir ao Encontro</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chêng</div>
+        <div class="hexagramSymbol">䷭</div>
+        <div>Ascensão</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Heng</div>
+        <div class="hexagramSymbol">䷟</div>
+        <div>Duração</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ching</div>
+        <div class="hexagramSymbol">䷯</div>
+        <div>O Poço</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ku</div>
+        <div class="hexagramSymbol">䷑</div>
+        <div>Trabalho Sobre o que Se Deteriorou</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Sun</div>
+        <div class="hexagramSymbol">䷸</div>
+        <div>A Suavidade</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ting</div>
+        <div class="hexagramSymbol">䷱</div>
+        <div>O Caldeirão</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ta Kuo</div>
+        <div class="hexagramSymbol">䷛</div>
+        <div>A Preponderâcia do Grande</div>
+    </div>
+    <div class="hexagramIndex">
+        ☲
+    </div>
+    <div class="hexagramItem">
+        <div>Tung Jên</div>
+        <div class="hexagramSymbol">䷌</div>
+        <div>Comunidade com os Homens</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ming I</div>
+        <div class="hexagramSymbol">䷣</div>
+        <div>O Obscurecimento da Luz</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Fêng</div>
+        <div class="hexagramSymbol">䷶</div>
+        <div>Abundância</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chi Chi</div>
         <div class="hexagramSymbol">䷾</div>
-        <div>O Receptivo</div>
+        <div>Após a Conclusão</div>
     </div>
     <div class="hexagramItem">
-        <div>Ch'ien</div>
-        <div class="hexagramSymbol">䷿</div>
-        <div>O Receptivo</div>
+        <div>Pi</div>
+        <div class="hexagramSymbol">䷕</div>
+        <div>Graciosidade</div>
     </div>
+    <div class="hexagramItem">
+        <div>Chia Jen</div>
+        <div class="hexagramSymbol">䷤</div>
+        <div>A Família</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Li</div>
+        <div class="hexagramSymbol">䷝</div>
+        <div>Aderir</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Ko</div>
+        <div class="hexagramSymbol">䷰</div>
+        <div>Revolução</div>
+    </div>
+    <div class="hexagramIndex">
+        ☱
+    </div>
+    <div class="hexagramItem">
+        <div>Lu</div>
+        <div class="hexagramSymbol">䷉</div>
+        <div>A Conduta</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Lin</div>
+        <div class="hexagramSymbol">䷒</div>
+        <div>Aproximação</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Kuei Mei</div>
+        <div class="hexagramSymbol">䷵</div>
+        <div>A Jovem que se Casa</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chieh</div>
+        <div class="hexagramSymbol">䷻</div>
+        <div>Limitação</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Sun</div>
+        <div class="hexagramSymbol">䷨</div>
+        <div>Diminuição</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Chung Fu</div>
+        <div class="hexagramSymbol">䷼</div>
+        <div>Verdade Interior</div>
+    </div>
+    <div class="hexagramItem">
+        <div>K'uei</div>
+        <div class="hexagramSymbol">䷥</div>
+        <div>Oposição</div>
+    </div>
+    <div class="hexagramItem">
+        <div>Tui</div>
+        <div class="hexagramSymbol">䷹</div>
+        <div>A Alegria</div>
+    </div>
+
+
+
+
+
+
 
 </div>
