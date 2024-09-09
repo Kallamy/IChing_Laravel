d77aef90cd1f6eab5c4b8dbe08a1bfb554a47552
Yuri Mallak <yurimallak@gmail.com>
2024-09-09 17:48:15 +0300

    fixing previous commit failure again

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1736,7 +1736,7 @@
             if(hexagrams[0].getNumber() != null) {
                 OriginalTextScreen.open("play");
                 if(hasTwoHexagrams) {
-                    OriginalTextScreen.write(hexagrams[0].getNumber(), hexagrams[1].getNumber(), [1,2,3,4,5,6]);
+                    OriginalTextScreen.write(hexagrams[0].getNumber(), hexagrams[1].getNumber(), movingLinesPositions);
                 } else {
                     OriginalTextScreen.write(hexagrams[0].getNumber());
                 }
