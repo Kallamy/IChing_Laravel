146a9e377f0efe80ba523781a2721d7d3549177b
Yuri Mallak <yurimallak@gmail.com>
2024-09-30 09:21:01 +0300

    login message in asks view

--- resources/views/asks.blade.php
+++ resources/views/asks.blade.php
@@ -1,6 +1,25 @@
 <x-layout page="asks">
     <section id="asks">
-        <h2>Registro de consultas</h2>
-        <h3>Esta área ainda não está disponível</h3>
+        <div class="content">
+            @if (session('locale') == 'en')
+                <div class="loginMessage">
+                    <h3>Consultation register</h3>
+                    <h4>To save your queries you need to be registered.</h4>
+                </div>
+                <div class="loginButtons">
+                    <x-Button onclick="window.location='{{ route('signup') }}'">Create User <img width="40.0rem" src="assets/icons/login-avatar.png"></x-Button>
+                    <x-Button onclick="window.location='{{ route('signin') }}'">Login <img width="40.0rem" src="assets/icons/login.png"></x-Button>
+                </div>
+            @elseif (session('locale') == 'pt')
+                <div class="loginMessage">
+                    <h3>Registro de consultas</h3>
+                    <h4>Para salvar suas consultas você precisa estar cadastrado.</h4>
+                </div>
+                <div class="loginButtons">
+                    <x-Button onclick="window.location='{{ route('signup') }}'">Criar Usuário <img width="40.0rem" src="assets/icons/login-avatar.png"></x-Button>
+                    <x-Button onclick="window.location='{{ route('signin') }}'">Entrar <img width="40.0rem" src="assets/icons/login.png"></x-Button>
+                </div>
+            @endif
+        </div>
     </section>
 </x-layout>
