c7c0ed7f05910e5c392b81bdc143f6d64da33826
Yuri Mallak <yurimallak@gmail.com>
2024-10-07 13:14:12 +0300

    creatig asks table migration

--- database/migrations/2024_10_07_100008_create_asks_table.php
+++ database/migrations/2024_10_07_100008_create_asks_table.php
@@ -11,21 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hexagrams', function (Blueprint $table) {
+        Schema::create('asks', function (Blueprint $table) {
             $table->id();
-            $table->integer('number');
-            $table->string('symbol');
-            $table->string('title');
-            $table->string('upper_trigram');
-            $table->string('bottom_trigram');
+            $table->date('date');
+            $table->string('subject');
+            $table->string('result');
+            $table->string('related');
+            $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('hexagrams');
+        Schema::dropIfExists('asks');
     }
 };
